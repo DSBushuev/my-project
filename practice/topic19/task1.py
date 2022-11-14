642ca1817f39d1f407eac195e65c4170df03a5b5
DSBushuev <deniwe1985@gmail.com>
2022-11-14 19:59:05 +0500

    task3 done

--- practice/topic19/task1.py
+++ practice/topic19/task1.py
@@ -2,26 +2,26 @@ import pygame
 
 pygame.init()
 
-W = 500
+W = 1000
 H = 500
 
-sc = pygame.display.set_mode((W, H))
+sc = pygame.display.set_mode((W, H), pygame.RESIZABLE)
 pygame.display.set_caption("task1-5")
 
 clock = pygame.time.Clock()
 FPS = 30
 
-R, G, B = 50, 50, 50
-COLOR = R,G,B
+R, G, B = 50, 250, 50
 BACKGROUND = (250,250,200)
 
 
-whith_ball = 50
-height_ball = 50
+whith_ball = 60
+height_ball = 60
 
 x = W // 2
 y = H // 2
 speed = 5
+friction_force = .9
 
 
 while 1:
@@ -31,8 +31,16 @@ while 1:
 
     keys = pygame.key.get_pressed()
 
-    if x < 1 or x > 499 - whith_ball:
+    if x < 1:
+        speed += 1
         speed *= -1
+        x = 1
+    elif x > W - whith_ball:
+        speed -= 1
+        speed *= -1
+        x = W - whith_ball - 1
+
+
 
     if keys[pygame.K_LEFT]:
         speed -= 1
@@ -40,11 +48,14 @@ while 1:
     elif keys[pygame.K_RIGHT]:
         speed += 1
 
+    speed -= 0.0098 * speed
 
-    x += speed // 2
+    x += int(speed)
+    if abs(speed) > 58:
+        speed = 58
 
     sc.fill(BACKGROUND)
-    ball = pygame.draw.ellipse(sc, COLOR,(x, y, whith_ball, height_ball) )
+    ball = pygame.draw.ellipse(sc,(int(R + abs(speed) * 3.5), int(G - abs(speed) * 3.5), B),(x, y, whith_ball, height_ball) )
     pygame.display.update()
 
     clock.tick(FPS)
